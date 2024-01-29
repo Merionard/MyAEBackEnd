cdcef957b0eeadc43afc4c8a5052cedd4b9a1e14
Mika <ext.mbenarab@librasoft.fr>
2024-01-29 11:22:43 +0100

    initProject

---
+++ src/main/java/mba/myAEBackEnd/MyAeBackEndApplication.java
@@ -0,0 +1,13 @@
+package mba.myAEBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAeBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mba/myAEBackEnd/MyAeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package mba.myAEBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
