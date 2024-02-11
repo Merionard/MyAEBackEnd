3e9086682ed1c9d66c9be9fba406f32ae992a198
Mika <ext.mbenarab@librasoft.fr>
2024-02-11 23:53:24 +0100

    cra fin

--- src/main/java/mba/myAEBackEnd/dto/CRA/WorkDayDto.java
+++ src/main/java/mba/myAEBackEnd/dto/CRA/WorkDayDto.java
@@ -5,11 +5,12 @@ import lombok.experimental.Accessors;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 @Data
 @Accessors(chain = true)
 public class WorkDayDto {
     private Long id;
-    private LocalDateTime date;
+    private ZonedDateTime date;
     private float duration;
 }
--- src/main/java/mba/myAEBackEnd/entity/WorkDay.java
+++ src/main/java/mba/myAEBackEnd/entity/WorkDay.java
@@ -7,6 +7,7 @@ import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 @Data
 @Entity
@@ -15,8 +16,8 @@ public class WorkDay {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
-    private LocalDateTime date;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy",timezone = "Europe/Paris")
+    private ZonedDateTime date;
     private float duration;
     @ManyToOne
     @JoinColumn(name = "line_id")
