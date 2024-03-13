c40d69a2fb7318fbddc285cdb57564f32441f118
Mika <ext.mbenarab@librasoft.fr>
2024-03-13 16:00:30 +0100

    fix cors and expose db

--- src/main/java/mba/myAEBackEnd/MyAeBackEndApplication.java
+++ src/main/java/mba/myAEBackEnd/MyAeBackEndApplication.java
@@ -18,8 +18,8 @@ public class MyAeBackEndApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/auth/**").allowedOrigins("http://localhost:5173");
-				registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET","PUT","POST","DELETE");
+				registry.addMapping("/auth/**").allowedOrigins("http://localhost:8081");
+				registry.addMapping("/**").allowedOrigins("http://localhost:8081").allowedMethods("GET","PUT","POST","DELETE");
 			}
 		};
 	}
